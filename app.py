ca9a19bc782b8dd6e36314764fb12e34791450b0
Eric Xia <eriq@Erics-MacBook-Air.local>
2023-06-06 16:32:11 -0400

    testing

--- app.py
+++ app.py
@@ -3,6 +3,5 @@ from waitress import serve
 app = create_app()
 
 if __name__ == "__main__": 
-    serve(app, host="0.0.0.0", port=5000)
+    serve(app, host="0.0.0.0", port=5050)
     #app.run(host='localhost', port=5050, debug=True)
-    
